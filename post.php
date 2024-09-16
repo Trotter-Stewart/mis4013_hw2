d7ea8d7a8bf86c7548430cdc1e5dec932659ef6c
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-09-16 11:59:26 -0500

    Update post.php

--- post.php
+++ post.php
@@ -1,8 +1,12 @@
 <?php
-$pageTitle = "Home";
+$pageTitle = "Post";
 include "view-header.php";
 ?>
-    <h1>Home</h1>
+<h1>Post</h1>
+<form method="post" action="post-result.php">
+    <input type = "text" name="my-name">
+    <input type= "submit" value="Post data">
+</form>
 <?php
 include "view-footer.php";
 ?>
