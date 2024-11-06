ea606b046ede63ff7e03310854f9ea5f1abe3731
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-06 12:35:31 -0600

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -4,19 +4,16 @@ include "view-header.php";
 ?>
 <h1>Post result</h1>
 
-<?php
+function getDisplay() {
+    if (isset($_POST['my-name']) && !empty($_POST['my-name'])) {
+        return "<p>The value sent is: " . htmlspecialchars($_POST['my-name']) . "</p>";
+    } else {
+        return "<p>Nothing posted to the page.</p>";
+    }
+}
+
 echo getDisplay();
+
 include "view-footer.php";
 
-function getDisplay()
-  {
-    if (isset($_POST['my-name'])) 
-{
-  return "<p>The value sent is: </p>" $_POST['my-name'];
-}
-else
-{
-  return "<p>Nothing posted to the the page.</p>";  
-}
-}
 ?>
