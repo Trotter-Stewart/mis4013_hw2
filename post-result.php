4648e60c58ddc87b509823263fe79f0d4ccd9631
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-06 12:36:28 -0600

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -3,7 +3,7 @@ $pageTitle = "Post result";
 include "view-header.php";
 ?>
 <h1>Post result</h1>
-
+<?php
 function getDisplay() {
     if (isset($_POST['my-name']) && !empty($_POST['my-name'])) {
         return "<p>The value sent is: " . htmlspecialchars($_POST['my-name']) . "</p>";
