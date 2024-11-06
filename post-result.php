7fac69f88c4aedef8ed4a839b4a65fee9f75a07b
Trotter-Stewart <112006521+Trotter-Stewart@users.noreply.github.com>
2024-11-06 12:26:49 -0600

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -12,7 +12,7 @@ function getDisplay()
   {
     if (isset($_POST['my-name'])) 
 {
-  return "<p>The value sent is: </p>"<?php . $_POST['my-name'];
+  return "<p>The value sent is: </p>" $_POST['my-name'];
 }
 else
 {
